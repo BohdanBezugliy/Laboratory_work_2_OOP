91bf64fc477978d8df605f137abedec58bdea668
Богдан Безуглый <winzar06031977@gmail.com>
2023-09-22 19:11:14 +0300

    Add project files.

---
+++ Laboratory_work_2_OOP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace First_task
+{
+    class Cube
+    {
+        public double edge { get; set; }
+        public bool Correct() 
+        {
+            return edge > 0;
+        }
+        public double Area() 
+        {
+            return 6 * Math.Pow(edge,2);
+        }
+        public double Volume()
+        {
+            return Math.Pow(edge, 3);
+        }
+        public void Print()
+        {
+            Console.WriteLine("Ребро куба дорівнює: " + edge);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding= Encoding.UTF8;
+            Cube cube = new Cube();
+            try
+            {
+                cube.edge = Convert.ToDouble(Console.ReadLine());
+                cube.Print();
+                if(cube.Correct())
+                {
+                    Console.WriteLine("Площа поверхні куба: " + cube.Area());
+                    Console.WriteLine("Об’єм куба: " + cube.Volume());
+                }
+                else
+                {
+                    Console.WriteLine("Такого куба не може існувати! Його ребро має бути додатнім і не дорівнювати нулю!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Наявне виключення: " + e.Message);
+            }
+        }
+    }
+}
